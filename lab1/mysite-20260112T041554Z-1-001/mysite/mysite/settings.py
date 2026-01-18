27ab852fe888669ebf58f4ef90ec378b0f309e30
Nisarg Desai <nisargdesai2036@gmail.com>
2026-01-19 00:41:55 +0530

    Added gitignore and cleaned repo

--- lab1/mysite-20260112T041554Z-1-001/mysite/mysite/settings.py
+++ lab1/mysite-20260112T041554Z-1-001/mysite/mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'firstapp',   # 👈 add this
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "firstapp.apps.FirstappConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
